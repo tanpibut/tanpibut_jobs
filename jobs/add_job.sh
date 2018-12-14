a4871ca365624303a538d2a6efa9f24150fa8a46
Krit <krit@HDA-Krit.mshome.net>
2018-12-14 11:36:03 +0700

    add TMD,D DWR, HAII jobs

---
+++ jobs/add_job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+BIGSTREAM_URL=bigstream:19980
+
+for file in *.json; do
+    echo "$file"
+    curl -X POST --data @"$file" -H "Content-Type:application/json" http://$BIGSTREAM_URL/v1.2/jobs
+    echo ""
+done
+echo "reload job : "
+curl -X GET http://$BIGSTREAM_URL/v1.2/jobs?reload=true
