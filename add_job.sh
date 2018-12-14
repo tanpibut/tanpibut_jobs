a75038690d5c7e9cfba44279bffc4226f5d2d6d0
Krit <krit@HDA-Krit.mshome.net>
2018-12-14 14:54:03 +0700

    fix path

--- add_job.sh
+++ add_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BIGSTREAM_URL=bigstream:19980
 
-for file in *.json; do
+for file in jobs/*.json; do
     echo "$file"
     curl -X POST --data @"$file" -H "Content-Type:application/json" http://$BIGSTREAM_URL/v1.2/jobs
     echo ""
